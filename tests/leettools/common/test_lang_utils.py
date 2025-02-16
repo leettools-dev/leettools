9dda31d81f0694871e40246c79b1c30bef9bf928
Feng Peng <pengfeng@gmail.com>
2025-02-16 10:09:44 -0800

    Use langid instead of langdetect for default language checking.

--- tests/leettools/common/test_lang_utils.py
+++ tests/leettools/common/test_lang_utils.py
@@ -8,12 +8,9 @@ def test_lang_utils():
     assert lan == "en"
 
     # Test case 2: zh-cn
-    # the following content returned value 'ca' from langdetect
-    # content = "这是一张图片: ![alt text](https://example"
-
-    content = "这是一张图片"
+    content = "这是一张图片: ![alt text](https://example"
     lan = get_language(content)
-    assert lan == "zh-cn"
+    assert lan == "zh"
 
     # Test case 3: ja
     content = "これは画像です: ![alt text](https://example"
@@ -23,16 +20,26 @@ def test_lang_utils():
     # Test case 4: spanish
     content = "Esta es una imagen"
     lan = get_language(content)
+    # langdetect returns 'ca' [Catalan] for this content, but it should be 'es'
+    assert lan == "es" or lan == "ca"
+
+    # Test case 5: spanish
+    content = "Hoy es un día maravilloso"
+    lan = get_language(content)
     assert lan == "es"
 
-    # Test case 5: french
+    # Test case 6: french
     # the following content returned value 'ro' from langdetect
-    # content = "Ceci est une image: ![alt text](https://example"
-    content = "Ceci est une image"
+    content = "Ceci est une image: ![alt text](https://example"
     lan = get_language(content)
     assert lan == "fr"
 
-    # Test case 6: mixed
+    # Test case 7: mixed
     content = "This is an image: ![alt text](https://example 这是一张图片"
+    lan = get_language(content)
+    assert lan == "en"
+
+    # Test case 8: empty
+    content = ""
     lan = get_language(content)
     assert lan == "en"
