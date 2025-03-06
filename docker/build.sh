2c58564d4e3f3c602225add93667b905cc5b443a
Feng Peng <pengfeng@gmail.com>
2025-03-05 18:46:18 -0800

    wip

--- docker/build.sh
+++ docker/build.sh
@@ -5,6 +5,8 @@ set -e -u
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASE_DIR="$(cd "$DIR"/.. && pwd)"
 
+org_name="leettools"
+app_name="leettools"
 # read the version number project.toml file and use in the docker image
 version=$(grep "^version = " "$BASE_DIR"/pyproject.toml | cut -d'"' -f2)
 
@@ -23,9 +25,9 @@ cd "$BASE_DIR"
 cp "$DIR"/dockerignore.template "$BASE_DIR"/.dockerignore
 cat "$BASE_DIR"/.gitignore >> "$BASE_DIR"/.dockerignore
 
-docker build -t leettools-dev/leettools:"${version}" -f "$DIR"/Dockerfile "$BASE_DIR"
+docker build -t "${org_name}/${app_name}":"${version}" -f "$DIR"/Dockerfile "$BASE_DIR"
 
 # tag the image with the latest version
-docker tag leettools-dev/leettools:"${version}" leettools-dev/leettools:latest
+docker tag "${org_name}/${app_name}":"${version}" "${org_name}/${app_name}":latest
 
 popd > /dev/null
